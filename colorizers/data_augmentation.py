7d289bcbc9148f403bd3c2bdb4d11a37cf7b2583
hoffkyle <hoffkyle@umich.edu>
2024-04-27 16:06:34 -0400

    Augmentation file downsamples and augments data

--- colorizers/data_augmentation.py
+++ colorizers/data_augmentation.py
@@ -13,7 +13,7 @@ images_ab = np.load('raw_data/ab1.npy')
 
 gray_array = []
 ab_array = []
-for i in range(1000):
+for i in range(500):
     image_gray = images_gray[i]
     image_ab = images_ab[i]
 
@@ -33,8 +33,22 @@ for i in range(1000):
     gray_array.append(img[:, :, 0])
     ab_array.append(img[:, :, 1:])
 
+for i in range(500, 1000):
+    image_gray = images_gray[i]
+    image_ab = images_ab[i]
+
+    img = np.zeros((224, 224, 3))
+    img[:, :, 0] = image_gray
+    img[:, :, 1:] = image_ab
+
+    size = (64, 64)   
+    img = cv2.resize(img, size, interpolation=cv2.INTER_AREA)
+
+    gray_array.append(img[:, :, 0])
+    ab_array.append(img[:, :, 1:])
+
 
 gray_array = np.array(gray_array)
 ab_array = np.array(ab_array)
-np.save(f'data/grayscale_rotated/grayscale.npy', gray_array)
-np.save(f'data/true_color_rotated/color.npy', ab_array)
+np.save(f'data/grayscale_downsample_aug/grayscale.npy', gray_array)
+np.save(f'data/true_color_downsample_aug/color.npy', ab_array)
